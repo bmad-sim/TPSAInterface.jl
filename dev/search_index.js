74a9f4f75663a2ccbf2fde38e9f25502d5cf8c7b
Documenter.jl <documenter@juliadocs.github.io>
2025-01-22 17:44:26 +0000

    build based on a00233b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TPSAInterface","category":"page"},{"location":"#TPSAInterface","page":"Home","title":"TPSAInterface","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TPSAInterface.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TPSAInterface]","category":"page"}]
+}
